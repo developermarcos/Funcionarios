6d05070e82c0c0013a4db83ccdfdd37648e97811
Marcos Lima <developermarcos@outlook.com>
2022-03-10 16:31:22 -0300

    Add project files.

---
+++ Funcionarios.ConsoleApp/Funcionario.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funcionarios.ConsoleApp
+{
+    public enum Departamentos
+    {
+        Administrativo, 
+        Operacional
+    }
+    internal class Funcionario
+    {
+        public string nome;
+        public Departamentos Departamento;
+        public decimal salario;
+        public DateTime dataCadastro;
+        public string rg;
+        public bool ehFuncionario;
+
+        public void Bonificar(decimal valor)
+        {
+            this.salario += valor;
+        }
+        public void Demitir()
+        {
+            this.ehFuncionario = false;
+        }
+        public void Mostrar()
+        {
+            Console.WriteLine("Funcionario: {0}", this.nome);
+            Console.WriteLine("Departamento: {0}", this.Departamento);
+            Console.WriteLine("Salário: {0}", this.salario.ToString("N2"));
+            Console.WriteLine("Data de cadastrar: {0}", this.dataCadastro);
+            Console.WriteLine("RG: {0}", this.rg);
+            Console.WriteLine("É funcionario: {0}", this.ehFuncionario == true? "Sim":"Não");
+        }
+    }
+}
---
+++ Funcionarios.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Funcionarios.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Funcionario marcos = new Funcionario();
+            marcos.nome = "Marcos";
+            marcos.salario = 5000.00m;
+            marcos.rg = "623.111.111";
+            marcos.ehFuncionario = true;
+            DateTime dataCadastro = new DateTime(2022,08,01);
+            marcos.dataCadastro = dataCadastro;
+            marcos.Departamento = Departamentos.Administrativo;
+            marcos.Bonificar(1000);
+            marcos.Mostrar();
+
+
+            Funcionario f1 = new Funcionario();
+            f1.nome = "Fiodor";
+            f1.salario = 100;
+            Funcionario f2 = new Funcionario();
+            f2.nome = "Fiodor";
+            f2.salario = 100;
+            if (f1 == f2)
+                Console.WriteLine("iguais");
+            else
+                Console.WriteLine("Diferentes");
+            
+            Console.ReadKey();
+        }
+    }
+}
